161570306db9f530c47325c02d1b1c033ae9c729
pr1s0n <li1111111@vip.qq.com>
2020-04-16 20:14:12 +0800

    修改部分md文件

---
+++ Python进阶/FunctionClass1.py
@@ -0,0 +1,29 @@
+def login():
+    print('login')
+def register():
+    print('register')
+def saveMoney():
+    print('SaveMoney')
+def withdraw():
+    print('WithDraw')
+
+funcDict = {
+    '0':['退出',None],
+    '1':['登录',login],
+    '2':['注册',register],
+    '3':['存款',saveMoney],
+    '4':['取款',withdraw]
+}
+while True:
+    for k in funcDict:
+        print(k,funcDict[k][0])
+    choice = input('请输入命令编号：').strip()
+    if not choice.isdigit():
+        print('必须输入编号')
+        continue
+    if choice == '0':
+        break
+    if choice in funcDict:
+        funcDict[choice][1]()
+    else:
+        print('输入指令不存在')
