f9d12f5720b7afd496978ecfa7317d011808a73a
pr1s0n <li1111111@vip.qq.com>
2020-05-10 23:29:04 +0800

    起始文件

---
+++ Python进阶/ATM/start.py
@@ -0,0 +1,14 @@
+# -*-coding:utf-8 -*-
+# @Author   ：pr1s0n
+# @Time     : 2020/5/10 20:57
+# @FileNAme : start.py
+# @Blog     : http://www.pr1s0n.com
+
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
+from core import src
+
+if __name__ == '__main__':
+    src.run()
+
