6405f9a657d78ce3d47cc9107cf5fe730d8f35f5
pr1s0n <li1111111@vip.qq.com>
2020-05-19 23:34:06 +0800

    新增md5加密和登录装饰器

--- Python进阶/ATM/lib/common.py
+++ Python进阶/ATM/lib/common.py
@@ -3,3 +3,28 @@
 # @Time     : 2020/5/7 10:37
 # @FileNAme : common.py
 # @Blog     : http://www.pr1s0n.com
+'''
+存放公共方法
+'''
+import hashlib
+# md5加密
+def get_pwd_md5(password):
+    md5_obj = hashlib.md5()
+    md5_obj.update(password.encode('utf-8'))
+    salt = '兔子先生'
+    md5_obj.update(salt.encode('utf-8'))
+    return md5_obj.hexdigest()
+
+# 登录装饰器
+
+def login_auth(func):
+    from core import src
+    def inner(*args,**kwargs):
+        if src.login_user:
+            res = func(*args,**kwargs)
+            return res
+        else:
+            print('用户还没有登录喔！')
+            src.login()
+            
+    return inner
