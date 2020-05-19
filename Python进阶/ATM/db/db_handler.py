466fab6a340708acc9adc1f0ada0e5ef6dffe17b
pr1s0n <li1111111@vip.qq.com>
2020-05-19 23:34:55 +0800

    验证用户数据存在

--- Python进阶/ATM/db/db_handler.py
+++ Python进阶/ATM/db/db_handler.py
@@ -13,9 +13,16 @@ def select(username):
     user_path = os.path.join(
         settings.USER_DATA_PATH,f'{username}.json'
     )
-    if os.path.exists():
-        pass
+    if os.path.exists(user_path):
+        with open(user_path,'r',encoding='utf-8') as f:
+            user_dic = json.load(f)
+            return user_dic
 
 
-def save():
-    pass
+def save(user_dic):
+    username = user_dic.get('username')
+    user_path = os.path.join(
+        settings.USER_DATA_PATH,f'{username}.json'
+    )
+    with open(user_path,'w',encoding='utf-8') as f:
+        json.dump(user_dic,f,ensure_ascii=False)
