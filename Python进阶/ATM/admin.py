2df6b1a56a8649d84c686fb2cb79d345b96c47bc
pr1s0n <li1111111@vip.qq.com>
2020-05-24 22:00:04 +0800

    管理员入口文件

---
+++ Python进阶/ATM/admin.py
@@ -0,0 +1,12 @@
+# -*-coding:utf-8 -*-
+# @Author   ：pr1s0n
+# @Time     : 2020/5/22 23:16
+# @FileNAme : admin.py
+# @Blog     : http://www.pr1s0n.com
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
+from core import admin
+
+if __name__ == '__main__':
+    admin.run()
